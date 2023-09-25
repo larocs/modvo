54295df729fc73aba2c842b2e61de01658ae4673
hudsonmartins <hudsonbr95@gmail.com>
2023-09-25 15:11:00 -0400

    debug: image callback

--- modvo/dataloaders/ros_stream.py
+++ modvo/dataloaders/ros_stream.py
@@ -15,9 +15,8 @@ class ROSStreamLoader(DataLoader):
         cam_info_topic = params['cam_info_topic']
         self.type = 'stream'
         
-        image_sub = message_filters.Subscriber(rgb_topic, CompressedImage)
-        cam_info_sub = message_filters.Subscriber(cam_info_topic, CameraInfo)
-        self.ts = message_filters.TimeSynchronizer([image_sub, cam_info_sub], 10)
+        image_sub = rospy.Subscriber(rgb_topic, CompressedImage, self.callback)
+        
         self.ts.registerCallback(self.callback)
         self.bridge = CvBridge()       
         self.buffer = []
@@ -34,20 +33,8 @@ class ROSStreamLoader(DataLoader):
                       'k3': 0}
         self.camera = PinholeCamera(**cam_params)
 
-    def callback(self, image, cam_info):
+    def callback(self, image):
         image = self.bridge.compressed_imgmsg_to_cv2(image)
-        cam_params = {'width': cam_info.width,
-                      'height': cam_info.height,
-                      'fx': cam_info.K[0], 
-                      'fy': cam_info.K[4], 
-                      'cx': cam_info.K[2],
-                      'cy': cam_info.K[5], 
-                      'k1': 0,
-                      'k2': 0,
-                      'p1': 0,
-                      'p2': 0,
-                      'k3': 0}
-        self.camera = PinholeCamera(**cam_params)
         self.buffer.append(image)
         if len(self.buffer) > self.buffer_size:
             self.buffer.pop(0)
