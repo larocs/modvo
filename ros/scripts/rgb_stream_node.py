f81b20934ac929770822aadfec57b3621441b9d4
hudsonmartins <hudsonbr95@gmail.com>
2023-10-23 11:00:53 -0400

    fix: camera parameters

--- ros/scripts/rgb_stream_node.py
+++ ros/scripts/rgb_stream_node.py
@@ -28,10 +28,10 @@ class RGBStreamingNode:
                                 frameHeightRGB   = height,
                                 frameRateRGB	 = rate,)
 
-        self.intrinsics = [967.22066, 0, 656.38549,
-                           0, 966.60926, 324.16049,
+        self.intrinsics = [912.469360351562, 0, 638.081298828125,
+                           0, 912.747497558594, 350.80615234375,
                            0, 0, 1]
-        self.distortion = [0.123865, -0.147635, -0.006330, 0.010211, 0.000000]
+        self.distortion = [0., 0., 0., 0., 0.]
 
     def run(self):
         rospy.on_shutdown(self.terminate_camera)
@@ -40,11 +40,10 @@ class RGBStreamingNode:
             self.publish_image()
             self.publish_cam_info()
             self.rate.sleep()
-            
-
+                      
     def publish_image(self):
         img_msg = CompressedImage()
-        img_msg.data = img_msg = self.bridge.cv2_to_compressed_imgmsg(self.rgb.imageBufferRGB)
+        img_msg.data = self.bridge.cv2_to_compressed_imgmsg(self.rgb.imageBufferRGB)
         img_msg.header.stamp =  rospy.Time.now()
         img_msg.header.frame_id = 'camera_link'
         self.imagePub.publish(img_msg)
@@ -60,8 +59,7 @@ class RGBStreamingNode:
         cam_info_msg.K = self.intrinsics
         cam_info_msg.D  = self.distortion
         self.camInfoPub.publish(cam_info_msg)
-        
-        
+    
     def terminate_camera(self):
         self.rgb.terminate()
 
